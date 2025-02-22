38a3b65b96191efaaef1ba9fe7f392496af48d6a
Anton Bryzghalov <absamurai@gmail.com>
2025-02-22 15:53:14 +0200

    Added/used function capitalizeFirstLetter. Img alt is changed in profile picture.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -10,7 +10,7 @@ export default function Profile({ name, tag, location, image, stats }) {
   return (
     <div className={css.profile}>
       <div className={css["main-user-info"]}>
-        <img src={image} alt={name} className={css.avatar} />
+        <img src={image} alt="User avatar" className={css.avatar} />
         <p className={css.name}>{name}</p>
 
         <p className={css.tag}>@{tag}</p>
--- src/components/TransactionRecord/TransactionRecord.jsx
+++ src/components/TransactionRecord/TransactionRecord.jsx
@@ -1,7 +1,9 @@
+import { capitalizeFirstLetter } from "../../js/utils";
+
 function TransactionRecord({ transaction }) {
   return (
     <>
-      <td>{transaction.type}</td>
+      <td>{capitalizeFirstLetter(transaction.type)}</td>
       <td>{transaction.amount}</td>
       <td>{transaction.currency}</td>
     </>
